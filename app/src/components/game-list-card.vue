75e612a9bce402add34bfd2d23de03665ea63f8a
Jacob Bare <jacob@parameter1.com>
2021-12-22 12:30:21 -0600

    Create and use game list card component

---
+++ app/src/components/game-list-card.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card h-100">
+    <div class="card-body flex-grow-0">
+      <h5 class="card-title mb-0 flex-shrink-1">
+        Type: {{ gameData.name }}
+      </h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li v-for="player in players" :key="player.colorId" class="list-group-item">
+        {{ player.name }}
+        [<span :style="`color: var(--bs-${player.colorId})`">{{ player.colorId }}</span>]
+      </li>
+    </ul>
+    <div class="card-body d-flex flex-column justify-content-end">
+      <div class="d-flex justify-content-between">
+        <button class="btn btn-sm btn-primary">
+          Manage
+        </button>
+      </div>
+    </div>
+    <div class="card-footer">
+      <small class="d-block text-muted">
+        ID: {{ timestamp }}
+      </small>
+      <small class="d-block text-muted">
+        Created: {{ createdDate }}
+      </small>
+    </div>
+  </div>
+</template>
+
+<script>
+import dayjs from '../dayjs';
+import gameData from '../../data';
+
+export default {
+  props: {
+    game: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    gameTypeId() {
+      return this.game.gameTypeId;
+    },
+    players() {
+      return this.game.players;
+    },
+    timestamp() {
+      return this.game.id;
+    },
+    createdDate() {
+      const date = new Date(this.timestamp);
+      return dayjs(date).format('MMM Do, YYYY @ HH:mma');
+    },
+    gameData() {
+      return gameData.get(this.gameTypeId);
+    },
+  },
+};
+</script>
--- app/src/pages/game-list.vue
+++ app/src/pages/game-list.vue
@@ -21,15 +21,31 @@
     </div>
     <hr>
   </div>
+
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
+    <div v-for="game in games" :key="game.id" class="col">
+      <game-list-card :game="game" />
+    </div>
+  </div>
 </template>
 
 <script>
 import NewGameDropdown from '../components/new-game-dropdown.vue';
+import GameListCard from '../components/game-list-card.vue';
 import storage from '../storage';
 
 export default {
   components: {
     NewGameDropdown,
+    GameListCard,
+  },
+
+  data: () => ({
+    games: [],
+  }),
+
+  mounted() {
+    this.games = storage.getAsArray('games');
   },
 
   methods: {
