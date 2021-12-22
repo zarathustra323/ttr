0b748b87bcab22913e5ab30cad7c58d738819ac4
Jacob Bare <jacob@parameter1.com>
2021-12-22 12:30:11 -0600

    Add getAsArray function

--- app/src/storage.js
+++ app/src/storage.js
@@ -3,6 +3,11 @@ const get = (key) => {
   return value ? JSON.parse(value) : null;
 };
 
+const getAsArray = (key) => {
+  const v = get(key);
+  return Array.isArray(v) ? v : [];
+};
+
 const set = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
@@ -10,19 +15,16 @@ const set = (key, value) => {
 const has = (key) => Boolean(localStorage.getItem(key));
 
 const push = (key, value) => {
-  const v = get(key);
-  if (!v) {
-    set(key, [value]);
-  } else {
-    v.push(value);
-    set(key, v);
-  }
+  const v = getAsArray(key);
+  v.push(value);
+  set(key, v);
 };
 
 const clear = () => localStorage.clear();
 
 export default {
   get,
+  getAsArray,
   set,
   has,
   push,
