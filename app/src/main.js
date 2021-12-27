418d3f96d42f99853b1cd55881a9b0b09a76b68b
Jacob Bare <jacob@parameter1.com>
2021-12-27 12:21:55 -0500

    Create not found page

--- app/src/main.js
+++ app/src/main.js
@@ -5,11 +5,13 @@ import App from './app.vue';
 import GameList from './pages/game-list.vue';
 import NewGame from './pages/new-game.vue';
 import ManageGame from './pages/manage-game.vue';
+import NotFound from './pages/not-found.vue';
 
 const routes = [
   { path: '/', component: GameList },
   { path: '/new-game/:typeId', component: NewGame },
   { path: '/game/:gameId', component: ManageGame },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
 
 const router = createRouter({
