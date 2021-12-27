f831a41c55ea36c421b85b1e56adedce1c166d95
Jacob Bare <jacob@parameter1.com>
2021-12-27 11:46:37 -0500

    Use web hash history (works with files)

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './app.vue';
 
 import GameList from './pages/game-list.vue';
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
