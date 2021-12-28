b0fec5d1f2071364aa09d24396295fe8777a9647
Jacob Bare <jacob@parameter1.com>
2021-12-27 19:03:37 -0500

    Add fullscreen support

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,58 @@
 <template>
   <div class="container py-3">
     <router-view />
+    <div class="mt-4">
+      <span v-if="isInFullsreen">
+        <a href="#" @click="exitFullscreen">
+          Exit Full Screen
+        </a>
+      </span>
+      <span v-else>
+        <a href="#" @click="enterFullscreen">
+          Enter Full Screen
+        </a>
+      </span>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    isInFullsreen: false,
+  }),
+
+  created() {
+    document.onfullscreenchange = ({ target }) => {
+      this.isInFullsreen = document.fullscreenElement === target;
+    };
+    document.onwebkitfullscreenchange = ({ target }) => {
+      this.isInFullsreen = document.webkitFullscreenElement === target;
+    };
+  },
+
+  methods: {
+    async enterFullscreen() {
+      const el = document.getElementsByTagName('html')[0];
+      if (el.requestFullscreen) {
+        await el.requestFullscreen();
+      } else if (el.webkitRequestFullscreen) {
+        await el.webkitRequestFullscreen();
+      }
+      this.isInFullsreen = true;
+    },
+
+    async exitFullscreen() {
+      if (document.exitFullscreen) {
+        await document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        await document.webkitExitFullscreen();
+      }
+      this.isInFullsreen = false;
+    },
+  },
+};
+</script>
 
 <style>
 body {
